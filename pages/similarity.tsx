2b543467b6ab010fbb424e9c9c26bbc14bedf107
Giorgi Keburia <keburius@Giorgis-MacBook-Pro.local>
2023-10-07 19:32:40 +0400

    update

--- pages/similarity.tsx
+++ pages/similarity.tsx
@@ -39,7 +39,7 @@ const Similarity: NextPage = () => {
       let config = {
         method: "post",
         maxBodyLength: Infinity,
-        url: "http://127.0.0.1:8000/search",
+        url: "https://similarity-api.ss.ge/search",
         headers: {
           "Content-Type": "application/json",
         },
