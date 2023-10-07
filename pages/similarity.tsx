b08797966e13ae19634a84902fe364e37435c5ee
Giorgi Keburia <keburius@Giorgis-MacBook-Pro.local>
2023-10-07 19:32:03 +0400

    update

--- pages/similarity.tsx
+++ pages/similarity.tsx
@@ -1,52 +1,56 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import axios from "axios";
+import {
+  Button,
+  FormControlLabel,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  Slider,
+  Switch,
+} from "@mui/material";
 
 const Similarity: NextPage = () => {
-  const [text, setText] = useState("");
-  const [confidence, setConfidence] = useState();
-  const [result, setResult] = useState("");
-  const [lang, setLang] = useState("DescriptionEn");
-  const [version, setVersion] = useState("new");
   const [isLoading, setIsLoading] = useState(false);
 
   const [imgUrl, setImgUrl] = useState("");
-  const [id, setId] = useState<number>();
+  const [imgId, setImgId] = useState<number>();
+  const [score, setScore] = useState<number>(90);
+  const [onlySearch, setOnlySearch] = useState(true);
+  const [limit, setLimit] = useState<number>();
 
-  const [message, setMessage] = useState("");
-  const [similar_id, setSimilarId] = useState("");
-  const [similarImgUrl, setSimilarImgUrl] = useState("");
-  const [score, setScore] = useState("");
+  const [similarImages, setSimilarImages] = useState<any>([]);
 
   const translateHandler = async () => {
-    if (!imgUrl?.length || !!!id) return;
+    if (!imgUrl?.length || !!!imgId) return;
     setIsLoading(true);
     try {
       let data = JSON.stringify({
-        ss_id: id,
-        path: imgUrl,
+        img_id: imgId,
+        img_path: imgUrl,
+        only_search: onlySearch,
+        limit: limit,
+        minimal_score: score / 100,
       });
 
       let config = {
         method: "post",
         maxBodyLength: Infinity,
-        url: "https://api-translate.ss.ge/search",
+        url: "http://127.0.0.1:8000/search",
         headers: {
           "Content-Type": "application/json",
         },
         data: data,
       };
 
-      const response = await axios.request(config);
-      setIsLoading(false);
-      console.log(response.data);
-      setSimilarImgUrl(response.data?.img_url);
-      setMessage(response.data?.message);
-      setSimilarId(response.data?.similar_id);
-      setScore(response.data?.score);
+      const response = (await axios.request(config)).data;
+      if (response?.success) {
+        setIsLoading(false);
+        setSimilarImages(response.data.similar_images);
+      }
     } catch (error) {
       setIsLoading(false);
       alert("error");
@@ -67,9 +71,9 @@ const Similarity: NextPage = () => {
         />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         {isLoading ? (
-          <div className="loader-cont">
+          <div className="loader-cont" style={{ zIndex: 100000 }}>
             <div className="loadingio-spinner-ripple-cmyt7s2fej9">
               <div className="ldio-8g16pyg0jxd">
                 <div></div>
@@ -80,53 +84,123 @@ const Similarity: NextPage = () => {
         ) : (
           <></>
         )}
-        <h1 className={"title"} style={{ color: "white" }}>
-          Similarity{" "}
-          <a className={"title-span"} href="https://nextjs.org">
-            Search
-          </a>
-        </h1>
 
-        <div className="similar-div">
-          <div className="similar-inputs-div">
-            <input
-              type="text"
-              className="similar-input"
-              placeholder="Paste Img Url"
-              onChange={(e) => setImgUrl(e.target.value)}
-              value={imgUrl}
-            />
-            <input
-              type="number"
-              className="similar-input"
-              placeholder="Enter Statement id"
-              onChange={(e) => setId(parseInt(e.target.value))}
-              value={id}
-            />
-          </div>
-          <button
-            className="my-button"
-            type="button"
-            onClick={() => translateHandler()}
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            height: "calc(100vh - 20px)",
+          }}
+        >
+          <div
+            className="similar-div"
+            style={{
+              width: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            Search
-          </button>
-        </div>
+            <div
+              style={{
+                width: "500px",
+                flexDirection: "column",
+                display: "flex",
+                gap: "20px",
+                // alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <h1 className={"title"} style={{ color: "white" }}>
+                Similarity{" "}
+                <a className={"title-span"} href="https://nextjs.org">
+                  Search
+                </a>
+              </h1>
+              <div className="similar-inputs-div">
+                <input
+                  type="number"
+                  className="similar-input"
+                  placeholder="Image ID"
+                  onChange={(e) => setImgId(parseInt(e.target.value))}
+                  value={imgId}
+                />
+                <input
+                  type="text"
+                  className="similar-input"
+                  placeholder="Image path"
+                  onChange={(e) => setImgUrl(e.target.value)}
+                  value={imgUrl}
+                />
+                <input
+                  type="number"
+                  className="similar-input"
+                  placeholder="Returned Img Count"
+                  onChange={(e) => setLimit(parseInt(e.target.value))}
+                  value={limit}
+                />
+              </div>
 
-        <div className="similar-result">
-          <div className="similar-result-img">
-            <img src={imgUrl} alt="" className="similar-img" />
-            <img src={similarImgUrl} alt="" className="similar-img" />
-          </div>
-          {message?.length ? (
-            <div className="similar-results">
-              <p className="similar-text">{message}</p>
-              <p className="similar-text">similar_id: {similar_id}</p>
-              <p className="similar-text">score: {score}</p>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={onlySearch}
+                    onChange={() => setOnlySearch(!onlySearch)}
+                    name="gilad"
+                  />
+                }
+                label="Only Search"
+                style={{
+                  marginTop: "20px",
+                }}
+              />
+
+              <FormControlLabel
+                control={
+                  <Slider
+                    defaultValue={score}
+                    onChange={(e: any) => setScore(e?.target?.value!)}
+                    aria-label="Default"
+                    valueLabelDisplay="auto"
+                  />
+                }
+                label="Similarity Rate"
+                labelPlacement="top"
+                style={{
+                  marginTop: "20px",
+                }}
+              />
+
+              <Button
+                variant="outlined"
+                style={{
+                  marginTop: 20,
+                }}
+                onClick={() => translateHandler()}
+              >
+                Search
+              </Button>
             </div>
-          ) : (
-            <></>
-          )}
+          </div>
+
+          <ImageList sx={{ width: "50%", height: "100%" }}>
+            {similarImages.map((img: any) => (
+              <ImageListItem sx={{ width: "100%", height: 400 }} key={img.id}>
+                <img
+                  srcSet={`${img.payload.img_path}?w=248&fit=crop&auto=format&dpr=3 1x`}
+                  src={`${img.payload.img_path}?w=248&fit=crop&auto=format`}
+                  alt={img.payload.img_path}
+                  loading="eager"
+                />
+                <ImageListItemBar
+                  style={{ color: "white" }}
+                  title={img.score}
+                  position="below"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
         </div>
       </main>
     </div>
